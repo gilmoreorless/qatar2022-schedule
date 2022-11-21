4ae822aafede41c3dcc1ee17db9d917465f91135
Gilmore Davidson <gilmoreorless@gmail.com>
2022-11-21 15:03:26 +1100

    Use relative paths in build config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ['**/*.tsv'],
-  plugins: [svelte()]
+  plugins: [svelte()],
+  base: './',
 })
